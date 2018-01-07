cb16336e156bc4226604d9bc0ba623650b57b50f
adyas <ashleydyas@gmail.com>
2018-01-07 17:08:07 -0500

    functional calculator, calculations triggered by operand click

--- out/hello_world/core.js
+++ out/hello_world/core.js
@@ -4,12 +4,81 @@ goog.require('cljs.core');
 goog.require('goog.dom.query');
 goog.require('clojure.string');
 goog.require('goog.dom');
+goog.require('goog.dom.safe');
 goog.require('goog.events');
 cljs.core.enable_console_print_BANG_.call(null);
 cljs.core.println.call(null,"Hello World");
-hello_world.core.handle_click = (function hello_world$core$handle_click(e){
-return cljs.core.println.call(null,"button pressed!");
+hello_world.core.add = (function hello_world$core$add(a,b){
+return (a + b);
 });
-goog.events.listen(goog.dom.getElement("enter"),goog.events.EventType.CLICK,hello_world.core.handle_click);
+hello_world.core.subtract = (function hello_world$core$subtract(a,b){
+return (a - b);
+});
+hello_world.core.multiply = (function hello_world$core$multiply(a,b){
+return (a * b);
+});
+hello_world.core.division = (function hello_world$core$division(a,b){
+return (a / b);
+});
+hello_world.core.handle_enter = (function hello_world$core$handle_enter(e){
+return cljs.core.println.call(null,"enter pressed!");
+});
+hello_world.core.handle_plus = (function hello_world$core$handle_plus(e){
+cljs.core.println.call(null,"plus pressed!");
+
+hello_world.core.v1 = parseInt(goog.dom.getElement("add-num-input-1").value);
+
+hello_world.core.v2 = parseInt(goog.dom.getElement("add-num-input-2").value);
+
+hello_world.core.sum = hello_world.core.add.call(null,hello_world.core.v1,hello_world.core.v2);
+
+cljs.core.println.call(null,hello_world.core.sum);
+
+return goog.dom.setTextContent(goog.dom.getElement("screen"),hello_world.core.sum);
+});
+hello_world.core.handle_minus = (function hello_world$core$handle_minus(e){
+cljs.core.println.call(null,"minus pressed!");
+
+hello_world.core.v1 = parseInt(goog.dom.getElement("sub-num-input-1").value);
+
+hello_world.core.v2 = parseInt(goog.dom.getElement("sub-num-input-2").value);
+
+hello_world.core.difference = hello_world.core.subtract.call(null,hello_world.core.v1,hello_world.core.v2);
+
+cljs.core.println.call(null,hello_world.core.difference);
+
+return goog.dom.setTextContent(goog.dom.getElement("screen"),hello_world.core.difference);
+});
+hello_world.core.handle_multiplication = (function hello_world$core$handle_multiplication(e){
+cljs.core.println.call(null,"mult pressed!");
+
+hello_world.core.v1 = parseInt(goog.dom.getElement("mult-num-input-1").value);
+
+hello_world.core.v2 = parseInt(goog.dom.getElement("mult-num-input-2").value);
+
+hello_world.core.product = hello_world.core.multiply.call(null,hello_world.core.v1,hello_world.core.v2);
+
+cljs.core.println.call(null,hello_world.core.product);
+
+return goog.dom.setTextContent(goog.dom.getElement("screen"),hello_world.core.product);
+});
+hello_world.core.handle_division = (function hello_world$core$handle_division(e){
+cljs.core.println.call(null,"div pressed!");
+
+hello_world.core.v1 = parseInt(goog.dom.getElement("div-num-input-1").value);
+
+hello_world.core.v2 = parseInt(goog.dom.getElement("div-num-input-2").value);
+
+hello_world.core.quotient = hello_world.core.division.call(null,hello_world.core.v1,hello_world.core.v2);
+
+cljs.core.println.call(null,hello_world.core.quotient);
+
+return goog.dom.setTextContent(goog.dom.getElement("screen"),hello_world.core.quotient);
+});
+goog.events.listen(goog.dom.getElement("enter"),goog.events.EventType.CLICK,hello_world.core.handle_enter);
+goog.events.listen(goog.dom.getElement("plus"),goog.events.EventType.CLICK,hello_world.core.handle_plus);
+goog.events.listen(goog.dom.getElement("minus"),goog.events.EventType.CLICK,hello_world.core.handle_minus);
+goog.events.listen(goog.dom.getElement("multiplication"),goog.events.EventType.CLICK,hello_world.core.handle_multiplication);
+goog.events.listen(goog.dom.getElement("division"),goog.events.EventType.CLICK,hello_world.core.handle_division);
 
 //# sourceMappingURL=core.js.map
--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -3,6 +3,7 @@
   (:require
    [clojure.string :refer [capitalize]]
    [goog.dom :as gdom]
+   [goog.dom.safe :as safe]
    [goog.events :as gevents]))
 
 (enable-console-print!)
@@ -10,17 +11,81 @@
 (println "Hello World")
 
 
-(fn add [a b] (+ a b))
+(defn add [a b] (+ a b))
 
-(fn subtract [a b] (- a b))
+(defn subtract [a b] (- a b))
 
-(fn multiply [a b] (* a b))
+(defn multiply [a b] (* a b))
 
-(fn divide [a b] (/ a b))
+(defn division [a b] (/ a b))
 
-(defn handle-click [e](println "button pressed!"))
+(defn handle-enter [e](println "enter pressed!"))
+
+(defn handle-plus [e](println "plus pressed!")
+(def v1 (js/parseInt (.-value (gdom/getElement "add-num-input-1"))))
+(def v2 (js/parseInt (.-value (gdom/getElement "add-num-input-2"))))
+
+
+(def sum (add v1 v2))
+(println sum)
+;(set! (.-value (gdom/getElement "screen")) sum)
+(gdom/setTextContent (gdom/getElement "screen") sum)
+)
+
+(defn handle-minus [e](println "minus pressed!")
+(set! v1 (js/parseInt (.-value (gdom/getElement "sub-num-input-1"))))
+(set! v2 (js/parseInt (.-value (gdom/getElement "sub-num-input-2"))))
+
+
+(def difference (subtract v1 v2))
+(println difference)
+;(set! (.-value (gdom/getElement "screen")) sum)
+(gdom/setTextContent (gdom/getElement "screen") difference)
+)
+
+(defn handle-multiplication [e](println "mult pressed!")
+(set! v1 (js/parseInt (.-value (gdom/getElement "mult-num-input-1"))))
+(set! v2 (js/parseInt (.-value (gdom/getElement "mult-num-input-2"))))
+
+
+(def product (multiply v1 v2))
+(println product)
+;(set! (.-value (gdom/getElement "screen")) sum)
+(gdom/setTextContent (gdom/getElement "screen") product)
+)
+
+(defn handle-division [e](println "div pressed!")
+(set! v1 (js/parseInt (.-value (gdom/getElement "div-num-input-1"))))
+(set! v2 (js/parseInt (.-value (gdom/getElement "div-num-input-2"))))
+
+
+(def quotient (division v1 v2))
+(println quotient)
+;(set! (.-value (gdom/getElement "screen")) sum)
+(gdom/setTextContent (gdom/getElement "screen") quotient))
 
 (gevents/listen
   (gdom/getElement "enter")
   (.-CLICK gevents/EventType)
-   handle-click)
+   handle-enter)
+
+(gevents/listen
+ (gdom/getElement "plus")
+ (.-CLICK gevents/EventType)
+  handle-plus)
+
+(gevents/listen
+  (gdom/getElement "minus")
+  (.-CLICK gevents/EventType)
+   handle-minus)
+
+(gevents/listen
+  (gdom/getElement "multiplication")
+  (.-CLICK gevents/EventType)
+    handle-multiplication)
+
+
+(gevents/listen
+  (gdom/getElement "division")
+  (.-CLICK gevents/EventType)
+    handle-division)
